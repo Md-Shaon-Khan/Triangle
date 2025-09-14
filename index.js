a1dab6de92f31825a8138713533d44ab31517bd4
Raunaq Jahan <raunaq.jahan60@gmail.com>
2025-09-14 20:02:33 +0600

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-fetch('./data.json');
+    fetch('./data.json')   // fetch your JSON file
+      .then(response => response.json())  // convert response to JS object
+      .then(data => {
+        const list = document.getElementById("memberList");
+
+        data.forEach(member => {
+          const li = document.createElement("li");
+          li.textContent = `ID: ${member.member_id}, Name: ${member.name}, Age: ${member.age}`;
+          list.appendChild(li);
+        });
+      })
+      .catch(error => console.error("Error fetching JSON:", error));
